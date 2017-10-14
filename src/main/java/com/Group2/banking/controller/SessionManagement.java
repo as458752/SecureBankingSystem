ab287fe41bbb249afaf192bfec2c284090b1904f
as458752 <as458752@gmail.com>
2017-10-13 21:37:59 -0700

    Add Cookie
    Add cookie for trusted device

--- src/main/java/com/Group2/banking/controller/SessionManagement.java
+++ src/main/java/com/Group2/banking/controller/SessionManagement.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package com.Group2.banking.controller;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  *
  * @author Jing
@@ -19,11 +21,31 @@ public class SessionManagement {
 	//setting session to expiry in 3 mins
 	session.setMaxInactiveInterval(3*60);
     }
+    
     public static String check(HttpServletRequest request)
     {
         HttpSession session = request.getSession();
         Object obj = session.getAttribute("userId");
         if (obj == null)    return "";
         else    return obj.toString();
+    }
+    
+    public static void addCookie(HttpServletRequest request, HttpServletResponse response, String userId)
+    {
+        Cookie loginCookie = new Cookie("user",userId);
+	//setting cookie to expiry in 100 days
+	loginCookie.setMaxAge(100*24*60*60);
+	response.addCookie(loginCookie);
+    }
+    
+    public static boolean checkCookie(HttpServletRequest request, String userId)
+    {
+        Cookie[] cookies = request.getCookies();
+        if(cookies !=null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("user") && userId.equals(cookie.getValue())) return true;
+            }
+        }
+        return false;
     }
 }
